e768992041998ec221be6f647fb718ce2c748364
wijohnst <wijohnst@gmail.com>
2025-03-11 15:19:33 -0700

    fix: updates accordion import

--- src/FAQ.tsx
+++ src/FAQ.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Accordion } from "./accordion";
+import { Accordion } from "./Accordion";
 import { Content } from "./Content";
 
 import { FAQContainer } from "./FAQ.styles";
