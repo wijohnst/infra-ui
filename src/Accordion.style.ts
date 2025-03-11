a186e91b5aad7c11e7dfa06a1e2baf4666afc6af
wijohnst <wijohnst@gmail.com>
2025-03-11 15:16:18 -0700

    feat: adds faq section

---
+++ src/Accordion.style.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { breakpoints } from "./style-vars";
+
+export const AccordionWrapper = styled.div`
+  border-radius: 4px;
+  margin-bottom: 10px;
+  padding: 1rem;
+  border: solid thin black;
+
+  box-shadow: var(--box-shadow);
+
+  button {
+    width: 100%;
+    background: unset;
+    border: none;
+    text-align: left;
+    cursor: pointer;
+
+    display: flex;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    flex-grow: 1;
+  }
+
+  @media (max-width: ${breakpoints.SM}) {
+    h2 {
+      font-size: 1rem;
+    }
+  }
+`;
